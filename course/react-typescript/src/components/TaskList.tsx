6a4b24ed39eb3361a9859ac38477cd65208256ed
nishidayoshikatsu <nsd24technology@gmail.com>
2021-12-13 16:22:15 +0900

    :hammer: solve problem 2-4

--- course/react-typescript/src/components/TaskList.tsx
+++ course/react-typescript/src/components/TaskList.tsx
@@ -7,12 +7,26 @@ type Props = {
 }
 export const TaskList: React.FC<Props> = ({ tasks, setTasks }) => {
   // Taskの状態を切り替える
+  const handleCheckBox = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    i: number
+  ) => {
+    const newTasks = tasks.map((task, _i) => {
+      return _i === i ? {...task, isDone: e.target.checked }: task;
+    });
+    setTasks(newTasks);
+  }
 
   return (
     <ul>
       {tasks.map((task, index) =>(
         <li key={`todo-${index}`}>
           {task.isDone ? <s>{task.label}</s> : task.label}
+          <input
+            onChange={(e) => handleCheckBox(e, index)}
+            type="checkbox"
+            checked={task.isDone}
+          />
         </li>
       ))}
     </ul>
