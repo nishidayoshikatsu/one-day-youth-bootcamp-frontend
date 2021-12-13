e3ffbfa4fbd3053d40bbe96bdf2686178454b9de
nishidayoshikatsu <nsd24technology@gmail.com>
2021-12-13 16:00:31 +0900

    :hammer: solve problem 2-1

--- course/react-typescript/src/components/TaskList.tsx
+++ course/react-typescript/src/components/TaskList.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Task } from '../';
 
+type Props = {
+  tasks: Task[];
+  setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
+}
 export const TaskList: React.FC = () => {
   // Taskの状態を切り替える
 
